a538b551831b9a871fb2c5938ebbcf33ded95007
Isabel Puerta <isabel.mia2244@gmail.com>
2024-11-25 12:03:39 -0500

    provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.38.0" # compatible version with default_tags
+      version = ">= 3.38.0" # compatible version with default_tags
     }
   }
 }
