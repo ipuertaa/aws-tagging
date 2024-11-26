687d25e5efd5ae7896e6b08591e56f07d42fd0a9
Isabel Puerta <isabel.mia2244@gmail.com>
2024-11-26 07:01:37 -0500

    test module without main. Just variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,18 @@
-# output "required_tags" {
-#     description = "Required Tags for AWS Resources"
-#     # value = {
-#     #     Owner = var.owner
-#     #     Application = var.application
-#     #     Environment = var.environment
-#     #     ManagedBy = var.managedBy
-#     # }
+output "required_tags" {
+    description = "Required Tags for AWS Resources"
+    # value = {
+    #     Owner = var.owner
+    #     Application = var.application
+    #     Environment = var.environment
+    #     ManagedBy = var.managedBy
+    # }
 
-#     value = merge(var.optional_tags,
-#     {
-#         Owner = var.owner
-#         Application = var.application
-#         Environment = var.environment
-#         ManagedBy = var.managedBy
-#     }
-#     )
-# }
+    value = merge(var.optional_tags,
+    {
+        owner = var.owner
+        application = var.application
+        environment = var.environment
+        managedBy = var.managedBy
+    }
+    )
+}
